d557bfd3237f85c85391a73b650c74383b1777b8
Mitali-laroia <04042001mitali@gmail.com>
2020-09-02 16:14:59 +0530

    Text Effect - 2

---
+++ skrew.js
@@ -0,0 +1,18 @@
+const section = document.querySelector("section");
+let currentPixel = window.pageYOffset
+
+//looper keeps running and keeps track of where the new pixel is
+const looper = function () {
+  const newPixel = window.pageYOffset;
+  const diff = newPixel - currentPixel
+  const speed = diff * 0.35;
+  
+  section.style.transform = "skewX(" + speed + "deg)"
+  
+  
+  currentPixel = newPixel;
+  
+  requestAnimationFrame(looper)
+}
+
+looper();
